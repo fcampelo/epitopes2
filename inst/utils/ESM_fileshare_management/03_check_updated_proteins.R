2b12a645c26d73599f260173aed0d7e569b5230a
Felipe Campelo <fcampelo@gmail.com>
2024-01-03 14:47:05 +0000

    Update 03_check_updated_proteins.R

--- inst/utils/ESM_fileshare_management/03_check_updated_proteins.R
+++ inst/utils/ESM_fileshare_management/03_check_updated_proteins.R
@@ -7,25 +7,29 @@ feat_path <- "d:/IEDB_proteins_ESM1b_features/features/"
 
 dirs <- dir(feat_path, full.names = TRUE)
 
-if(Sys.info()["sysname"] == "Windows"){
-  cl <- parallel::makeCluster(ncpus, setup_timeout = 2)
-  parallel::clusterExport(cl, c("dirs"))
-} else {
-  cl <- ncpus
+errlist <- data.frame(folder = character(),
+                      file   = character())
+
+for (j in 46:75){
+  cat("\n")
+  t0 <- Sys.time()
+  fl <- dir(dirs[j], full.names = TRUE)
+  fn <- dir(dirs[j], full.names = FALSE)
+  for (i in seq_along(fl)){
+    cat(sprintf("\rFolder %02d/%02d file %03d/%03d",
+                j, length(dirs), i, length(fl)))
+    X <- readRDS(fl[i])
+    if(!all(c("Info_pos", "Info_AA") %in% names(X))){
+      cat("\t *")
+      errlist <- rbind(errlist,
+                       data.frame(folder = i,
+                                  file   = fn[i]))
+    }
+  }
+  a <- difftime(Sys.time(), t0)
+  cat(":", as.numeric(a), attr(a, "units"))
+
 }
 
-errs <- pblapply(seq_along(dirs),
-                 function(i){
-                   fl <- dir(dirs[i], full.names = TRUE)
-                   fn <- dir(dirs[i], full.names = FALSE)
-                   errlist <- data.frame(folder = character(),
-                                         file   = character())
-                   for (i in seq_along(fl)){
-                     X <- readRDS(fl[i])
-                     if(!all(c("Info_pos", "Info_AA") %in% names(X))){
-                       errlist <- rbind(errlist,
-                                        data.frame(folder = i,
-                                                   file   = fn[i]))
-                     }
-                   }
-                 }, cl = cl)
+errlist
+
