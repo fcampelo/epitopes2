91162f8cc92dd2127c1c59675ff6e2503024b30b
Felipe Campelo <fcampelo@gmail.com>
2025-02-03 16:37:36 +0000

    Updated documentation of set_mc_cluster()

--- R/set_mc_cluster.R
+++ R/set_mc_cluster.R
@@ -1,7 +1,14 @@
 #' Set multicore cluster
 #'
-#' @param ncpus number of ores to use
+#' This function is a wrapper around [parallel::makePSOCKcluster()] followed
+#' by [parallel::clusterEvalQ()], which loads all required packages in the
+#' individual cluster workers.
 #'
+#' @param ncpus number of cores to use
+#'
+#' @return a SOCK cluster object
+#'
+#' @export
 #'
 set_mc_cluster <- function(ncpus){
   cl <- parallel::makePSOCKcluster(names = ncpus, setup_timeout = 2)
